11204206de4dfa53ac617c7761fae36525f995a7
lmasure <laurent@masure.pro>
2021-04-23 17:43:07 +0200

    commit du vendredi

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ if ( !function_exists( 'child_theme_configurator_css' ) ):
         wp_enqueue_style( 'chld_thm_cfg_ext5', trailingslashit( get_theme_root_uri() ) . 'hello-child/styles/eventbrite.css' );
         wp_enqueue_style( 'chld_thm_cfg_ext6', trailingslashit( get_theme_root_uri() ) . 'hello-child/styles/home.css' );
         wp_enqueue_style( 'chld_thm_cfg_ext7', trailingslashit( get_theme_root_uri() ) . 'hello-child/styles/youtube.css' );
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style' ) );
+        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style' ) );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 999 );
